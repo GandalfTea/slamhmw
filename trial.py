5c58b456614d952ef54a90017270a16bc9b8a9be
Octavian Rusu <octavian.concept@gmail.com>
2022-01-06 12:09:42 +0000

    camera calibration is done, F is guessed at 450 using the normalized coords

--- trial.py
+++ trial.py
@@ -8,14 +8,17 @@ import numpy as np
 import sys
 from skimage.measure import ransac 
 from skimage.transform import FundamentalMatrixTransform
+from skimage.transform import EssentialMatrixTransform
+
+np.set_printoptions(suppress=True)
 
 # video split
-W = 1280
+W = 1280 
 H = 720 
 
 # 254, 240
-GW = 254
-GH = 240
+GW = 112 
+GH = 126
 
 if len(sys.argv) < 2:
     print("Error: Video argument not found.\n")
@@ -30,18 +33,26 @@ if (vid.isOpened() == False):
     print("Error opening video.");
     sys.exit()
 
+# turn [x, y] -> [x, y, 1]
+def pad(p):
+    return np.concatenate([p, np.ones((p.shape[0], 1))], axis=1)
 
 class Frame(object):
-    def __init__(self, img, kp, des):
+    def __init__(self, img, kp, des, F, K, Kinv):
         self.img = img
         self.kp = kp
         self.des = des
+        self.F = F
+        self.K = K 
+        self.Kinv = Kinv 
 
 
 frames = [] 
 while(vid.isOpened()):
-    start = time.time()
+    start_full = time.time()
+
     ret, frame = vid.read()
+    frame = cv2.resize(frame, (896, 504), interpolation = cv2.INTER_AREA)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     if len(frames) > 2: frames.pop(0)
@@ -57,8 +68,18 @@ while(vid.isOpened()):
                     # recalibrating data 
                     kp = [cv2.KeyPoint(x=f[0][0] + rw, y=f[0][1] + rh, size=20) for f in pts]
                     kps.extend(kp)
-        kp, des = sift.compute(gray, kps)
-        frames.append(Frame(gray, kps, des))
+
+        _, des = sift.compute(gray, kps)
+
+        # by model values, it should be 100
+        F = 450
+        K = np.array([[F, 0, gray.shape[1]//2],
+                      [0, F, gray.shape[0]//2],
+                      [0, 0, 1]])
+        Kinv = np.linalg.inv(K)
+
+        frames.append(Frame(gray, kps, des, F, K, Kinv))
+
 
         if len(frames) > 1:
             # matches
@@ -76,32 +97,45 @@ while(vid.isOpened()):
 
             if len(good) > 8:
                 good = np.array(good)
-                #print(good.shape)
+
+                # Normalized coordinates are independent of the resolution of the image and give better 
+                # numerical stability for some multi-view geometry algorithms than pixel coordinates.
+                # Normalized points are stored in values -1 - 1, with 1 being the larger side of the picture
+                # The origin is the middle of the image. 
+                # first one is changing the queryIdx, second trainIdx
+
+                # order of change is queryIdx followed by trainIdx
+
+                # pad() adds a 1 at the end for shape but [:, 0:2] removes it in the end
+                good[:, 0, :] = np.dot(Kinv, pad(good[:, 0, :]).T).T[:, 0:2]
+                good[:, 1, :] = np.dot(Kinv, pad(good[:, 1, :]).T).T[:, 0:2]
                 
                 model, inliers = ransac((good[:, 0], good[:, 1]),
-                                        FundamentalMatrixTransform,
+                                        EssentialMatrixTransform,
                                         min_samples=8,
-                                        residual_threshold=1,
-                                        max_trials=100)
+                                        residual_threshold=0.5,
+                                        max_trials=200)
                 good = good[inliers]
-                #print(sum(inliers))
+                #print(sum(inliers), " matches")
+
 
             for (kp1, kp2) in good:
-                kp1s = list(map(lambda x: int(round(x)), kp1))
-                kp2s = list(map(lambda x: int(round(x)), kp2))
-                cv2.line(frame, kp1s, kp2s, (255,0,0), 2)
+                # denormalize the points
+                c1 = np.dot(K, np.array([kp1[0], kp1[1], 1.0]))
+                c2 = np.dot(K, np.array([kp2[0], kp2[1], 1.0]))
 
-        for p in kps:
-            x, y = int(p.pt[0]), int(p.pt[1]) 
-            cv2.circle(frame, (x, y),1,(0,255,0), 1)
+                x1, y1 = int(round(c1[0])), int(round(c1[1]))
+                x2, y2 = int(round(c2[0])), int(round(c2[1]))
+                cv2.circle(frame, (x1, y1),1,(0,255,0), 1)
+                cv2.line(frame, (x1, y1), (x2, y2), (255,0,0), 1)
 
         cv2.imshow('Frame', frame)
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     else:
         break
-    stop = time.time()
-    print(stop-start)
+    #stop = time.time()
+    #print("FULL FRAME: ", stop-start_full, "\n")
 
 vid.release()
 cv2.destroyAllWindows()
